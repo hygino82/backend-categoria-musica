dda0847ecc968347b8cc72d466e1e10c55b23e8c
Adroaldo <hygino@msn.com>
2024-08-06 19:28:54 -0300

    PUT: Gravação

--- src/main/java/com/utfpr/controller/v2/GravacaoControllerV2.java
+++ src/main/java/com/utfpr/controller/v2/GravacaoControllerV2.java
@@ -1,24 +1,16 @@
 package com.utfpr.controller.v2;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.utfpr.entity.Gravacao;
 import com.utfpr.entity.Gravacao;
 import com.utfpr.service.GravacaoService;
-import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v2/gravacao")
@@ -26,8 +18,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class GravacaoControllerV2 {
     private static final Logger log = LoggerFactory.getLogger(GravacaoControllerV2.class);
 
-    @Autowired
     private GravacaoService service;
+
+    public GravacaoControllerV2(GravacaoService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public ResponseEntity<List<Gravacao>> getAll() {
@@ -50,7 +45,7 @@ public class GravacaoControllerV2 {
     @PutMapping("/{id}")
     public ResponseEntity<Gravacao> update(@PathVariable(value = "id") Long id, @RequestBody Gravacao gravacaoUpdated) {
         Optional<Gravacao> gravacaoOld = this.service.encontrar(id);
-        log.warn("Tente put endpoint");
+        log.warn("Teste put endpoint");
         log.warn("Gravacao: {}", gravacaoUpdated);
         if (gravacaoOld.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
